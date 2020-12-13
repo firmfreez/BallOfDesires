34c30e928c1aa20955985b060ec83c87bc58d43c
firmfreez <aleksanyanc@list.ru>
2020-12-14 01:46:06 +0300

    Add pull request bug fixes Redesign TextDialogFragment for best quality display

--- app/src/main/java/com/firmfreez/android/ballofdesires/view/dialogs/TextDialogFragment.kt
+++ app/src/main/java/com/firmfreez/android/ballofdesires/view/dialogs/TextDialogFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
@@ -30,9 +31,67 @@ class TextDialogFragment: DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentTextDialogBinding.inflate(inflater, container, false)
+        //Скрываем заголовок, чтоб не оставлять белого пространства
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initDissmisMethod()
+        initApplyButton()
+        initCancelButton()
+        initIcon()
+        initTitle()
+        initMessage()
+    }
+
+    private fun initDissmisMethod() {
         dismissEnabled = arguments?.getBoolean(DISMISS_ENABLED) ?: true
+    }
 
+    private fun initMessage() {
+        arguments?.getInt(MESSAGE_ARG)?.let {
+            if (it == 0) {
+                binding.message.isVisible = false
+            } else {
+                binding.message.setText(it)
+            }
+        }
+    }
+
+    private fun initTitle() {
+        arguments?.getInt(TITLE_ARG)?.let {
+            if (it == 0) {
+                binding.title.isVisible = false
+            } else {
+                binding.title.setText(it)
+            }
+        }
+    }
+
+    private fun initIcon() {
+        arguments?.getInt(DRAWABLE_ARG)?.let {
+            if (it == 0) {
+                binding.icon.isVisible = false
+            } else {
+                binding.icon.setImageDrawable(ContextCompat.getDrawable(requireContext(), it))
+            }
+        }
+    }
+
+    private fun initCancelButton() {
+        if (arguments?.getBoolean(CANCEL_BUTTON_ENABLED, true) == true) {
+            binding.cancel.setOnClickListener {
+                router.sendResult(TEXT_DIALOG_RESULT, CANCEL)
+                dismiss()
+            }
+        } else {
+            binding.cancel.isVisible = false
+        }
+    }
+
+    private fun initApplyButton() {
         if(arguments?.getBoolean(APPLY_BUTTON_ENABLED, true) == true){
             binding.submit.setOnClickListener {
                 router.sendResult(TEXT_DIALOG_RESULT, APPLY)
@@ -41,45 +100,6 @@ class TextDialogFragment: DialogFragment() {
         } else {
             binding.submit.isVisible = false
         }
-
-        if(arguments?.getBoolean(CANCEL_BUTTON_ENABLED, true) == true){
-            binding.cancel.setOnClickListener {
-                router.sendResult(TEXT_DIALOG_RESULT, CANCEL)
-                dismiss()
-            }
-        } else {
-            binding.cancel.isVisible = false
-        }
-
-        //Получаем иконку, если её нет - скрываем объект иконки
-        arguments?.getInt(DRAWABLE_ARG)?.let {
-            if(it == 0) {
-                binding.icon.isVisible = false
-            } else {
-                binding.icon.setImageDrawable(ContextCompat.getDrawable(requireContext(), it))
-            }
-        }
-
-        //Получаем заголовок, если его нет - скрываем TextView
-        arguments?.getInt(TITLE_ARG)?.let {
-            if(it == 0) {
-                binding.title.isVisible = false
-            } else {
-                binding.title.setText(it)
-            }
-        }
-
-        //Получаем сообщение, если его нет - скрываем TextView
-        arguments?.getInt(MESSAGE_ARG)?.let {
-            if(it == 0) {
-                binding.message.isVisible = false
-            } else {
-                binding.message.setText(it)
-            }
-        }
-
-
-        return binding.root
     }
 
     override fun onDismiss(dialog: DialogInterface) {
